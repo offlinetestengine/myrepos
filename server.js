a7aa9adb984c62a15b72c7cf3a14b674deccb853
offlinetestengine <offlinetestengine@gmail.com>
2016-11-05 12:58:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var mongodb = require('mongodb');
 var http = require("http");
 var url = require("url");
 var show = {};
-var port;
 var MongoClient = mongodb.MongoClient;
 var addr = 'mongodb://db1:root@ds011873.mlab.com:11873/offlinedb';
 MongoClient.connect(addr, function (err, db) {
@@ -27,10 +26,11 @@ MongoClient.connect(addr, function (err, db) {
 });
 http.createServer(function (req, res)
     {
-   // port=Number(process.env.PORT||5000)
+  var  port=Number(process.env.PORT||2332)
         res.writeHead(200, {
             'Content-Type': 'application/json'
         });
         res.write(show);
         res.end(" ");
-    }).listen(2332);
+    }).listen(port);
+console.log("2332");
