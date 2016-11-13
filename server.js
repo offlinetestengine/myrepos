badf95b902c1bd2af14c295d625792ce15ad1bc8
offlinetestengine <offlinetestengine@gmail.com>
2016-11-13 13:12:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-//lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
-var express = require('express');
-var http = require("http");
-var url = require("url");
+var express = require('express')
+var app = express()
+var mongodb = require('mongodb')
+var http = require("http")
+var url = require("url")
 var show = {};
+var port;
 var MongoClient = mongodb.MongoClient;
 var addr = 'mongodb://db1:root@ds011873.mlab.com:11873/offlinedb';
 
-var app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-MongoClient.connect(addr, function (err, db) {
+app.get('/', function (req, res) {
+    
+    
+  MongoClient.connect(addr, function (err, db) {
     if (err) {
         console.log('Unable to connect to the mongoDB server. Error:', err);
     } else {
@@ -25,7 +28,9 @@ MongoClient.connect(addr, function (err, db) {
             if (err) {
                 console.log(err);
             } else if (result.length) {
-                show = JSON.stringify(result);   
+                show = JSON.stringify(result); 
+                res.send(show);
+                console.log("good to go"); 
             } else {
                 console.log('No document(s) found with defined "find" criteria!');
             }         
@@ -33,13 +38,15 @@ MongoClient.connect(addr, function (err, db) {
         });
     }
 });
-http.createServer(function (req, res)
-    {
-//  var  port=Number(process.env.PORT||2332)
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-        });
-        res.write(show);
-        res.end(" ");
-    }).listen(process.env.PORT || 5000);
-console.log("5000");
+  
+
+})
+
+ port=Number(process.env.PORT||1337)
+app.listen(port, function () {
+    
+    
+ // console.log('Example app listening on port 3000!')
+
+})
+
